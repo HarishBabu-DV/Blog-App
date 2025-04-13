4fa99c15944327a061c73aa6307b0bdec1c8fd4a
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-04-13 17:05:31 +0530

    modified ui no of blogs count in admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -19,12 +19,12 @@ const page = () => {
       <h1 className='text-2xl font-medium text-center py-1.5'>Welcome Back Admin &#128075;</h1>
 
 
-      <div>
+      <div className='flex flex-col items-center shadow-xl w-max rounded-md px-4 py-6 border-[1px] border-gray-300 '>
         <div>
-          <iconContents.bookIcon className='text-3xl'/>
+          <iconContents.bookIcon className=' text-blue-700 text-3xl'/>
         </div>
-        <h2>Total Blogs Created</h2>
-        <p>{blogDetails.length}</p>
+        <h2 className='text-xl text-gray-500 font-medium'>Total Blogs Created</h2>
+        <p className='text-xl font-bold text-blue-700'>{blogDetails.length}</p>
       </div>
     </section>
   )
